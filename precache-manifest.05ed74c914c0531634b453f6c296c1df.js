112402027c16992b3ec3c1d3a0d2183e0d578813
AchalShah1006 <shahachal.32@gmail.com>
2020-10-01 20:34:39 +0530

    Updates

---
+++ precache-manifest.05ed74c914c0531634b453f6c296c1df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe053299a9053106da2e59cc507e3be",
+    "url": "/colorPicker/index.html"
+  },
+  {
+    "revision": "c96eed3783b026fcdd8b",
+    "url": "/colorPicker/static/css/main.c0e53f7a.chunk.css"
+  },
+  {
+    "revision": "f8a1c0f68cb8e9828892",
+    "url": "/colorPicker/static/js/2.33e8e276.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/colorPicker/static/js/2.33e8e276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c96eed3783b026fcdd8b",
+    "url": "/colorPicker/static/js/main.2478dff8.chunk.js"
+  },
+  {
+    "revision": "0ea632c81c7c9a409836",
+    "url": "/colorPicker/static/js/runtime-main.a5d0f2d2.js"
+  }
+]);
